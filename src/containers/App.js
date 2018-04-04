c168964343f69403a227fadbf98e7345b35a4afe
Your Name <fisnikcanaj2@gmail.com>
2018-04-04 11:35:28 +0200

    Convert stateless to stateful components

--- src/containers/App.js
+++ src/containers/App.js
@@ -48,7 +48,7 @@ class App extends Component {
     let persons = null;
     
     if (this.state.showPersons) {
-      persons = < Persons 
+      persons = <Persons 
             persons={this.state.persons}
             clicked={this.deletePersonHandler}
             changed={this.nameChangedHandler}
